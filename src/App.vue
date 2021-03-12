277a5d5ced37b3171f28654370558642599d4044
Shinigami92 <chrissi92@hotmail.de>
2021-03-12 17:55:34 +0100

    pug v-slot vite error

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,9 @@
 <script lang="ts">
 import logo from '@/assets/logo.png';
 import { defineComponent } from '@vue/composition-api';
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  },
   setup() {
     return { logo };
   },
@@ -18,14 +14,3 @@ export default defineComponent({
   }
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import vuetify from '@/plugins/vuetify';
+import VueCompositionApi from '@vue/composition-api';
 import Vue from 'vue';
 import App from './App.vue';
-import VueCompositionApi from '@vue/composition-api';
 
 Vue.use(VueCompositionApi);
 
 new Vue({
+  vuetify,
   render: (h) => h(App)
 }).$mount('#app');
