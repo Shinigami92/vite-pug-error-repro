67cb7acdcffef8032978ed2ccddef19625451996
Shinigami92 <chrissi92@hotmail.de>
2021-03-12 16:40:48 +0100

    Working alias

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template lang="pug" src="./App.pug"></template>
+
+<script lang="ts">
+import logo from '@/assets/logo.png';
+import { defineComponent } from '@vue/composition-api';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    return { logo };
+  },
+  created() {
+    console.log(logo);
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
